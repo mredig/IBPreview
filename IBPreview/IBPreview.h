7a80e8b05693be3ed6855690f5b477ac2b54d0de
Michael Redig <mredig@gmail.com>
2019-11-19 10:59:18 -0600

    initial commit

---
+++ IBPreview/IBPreview.h
@@ -0,0 +1,19 @@
+//
+//  IBPreview.h
+//  IBPreview
+//
+//  Created by Michael Redig on 11/19/19.
+//  Copyright © 2019 Michael Redig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IBPreview.
+FOUNDATION_EXPORT double IBPreviewVersionNumber;
+
+//! Project version string for IBPreview.
+FOUNDATION_EXPORT const unsigned char IBPreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBPreview/PublicHeader.h>
+
+
